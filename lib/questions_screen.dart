2a734550fd4ff1bda116c00563afc9bfd9849349
Hyunwoo Cho <barefoot1423@gmail.com>
2023-10-25 22:26:48 -0400

    commit lecture 67

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
-
   const QuestionsScreen({super.key});
 
   @override
@@ -13,7 +12,27 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('QuestionsScreen');
+    return Column(
+      children: const [
+        Text('The question...'),
+        SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Answer 1'),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Answer 2'),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Answer 3'),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Answer 4'),
+        ),
+      ],
+    );
   }
-
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -7,44 +7,48 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-            Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 300,
-              color: const Color.fromARGB(150, 255, 255, 255),
+    return SizedBox(
+      width: double.infinity,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+              Image.asset(
+                'assets/images/quiz-logo.png',
+                width: 300,
+                color: const Color.fromARGB(150, 255, 255, 255),
+              ),
+            // Opacity(
+            //   opacity: 0.6,
+            //   child: Image.asset(
+            //     'assets/images/quiz-logo.png',
+            //     width: 300,
+            //   ),
+            // ),
+            const SizedBox(
+              height: 80,
             ),
-          // Opacity(
-          //   opacity: 0.6,
-          //   child: Image.asset(
-          //     'assets/images/quiz-logo.png',
-          //     width: 300,
-          //   ),
-          // ),
-          const SizedBox(
-            height: 80,
-          ),
-          const Text(
-            'Learn Flutter the fun way!',
-            style: TextStyle(
-              color: Color.fromARGB(255, 237, 237, 232),
-              fontSize: 24,
+            const Text(
+              'Learn Flutter the fun way!',
+              style: TextStyle(
+                color: Color.fromARGB(255, 237, 237, 232),
+                fontSize: 24,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          OutlinedButton.icon(
-            onPressed: startQuiz,
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.white,
+            const SizedBox(
+              height: 30,
             ),
-            icon: Icon(Icons.arrow_right_alt),
-            label: Text('Start Quiz'),
-          ),
-        ],
+            OutlinedButton.icon(
+              onPressed: startQuiz,
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              icon: Icon(Icons.arrow_right_alt),
+              label: Text('Start Quiz'),
+            ),
+          ],
+        ),
       ),
     );
   }
