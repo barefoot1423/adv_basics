28d37d739dff72be62ed62c48f84f9a97a9103ec
Hyunwoo Cho <barefoot1423@gmail.com>
2023-11-03 20:30:20 -0400

    commit lecture 84

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary(this.summaryData, {super.key});
 
@@ -10,7 +11,17 @@ class QuestionsSummary extends StatelessWidget {
     return Column(
       children: summaryData.map((data) {
         return Row(
-          children: [Text(((data['question'] as int) + 1) as String)],
+          children: [
+            Text(((data['question_index'] as int) + 1).toString()),
+            Column(
+              children: [
+                Text(data['question'] as String),
+                SizedBox(height: 5,),
+                Text(data['user_answer'] as String),
+                Text(data['correct_answer'] as String),
+              ],
+            ),
+          ],
         );
       }).toList(),
     );
